3bf155a0a58cc0a8c0c34a66e6688171a56b94cb
Gustavo Ferreira <gfmb94@gmail.com>
2022-11-21 19:51:13 -0300

    Criando classe veiculo, lista cadastro e construtor

---
+++ Aula02/CadastroVeiculo.py
@@ -0,0 +1,11 @@
+import os
+
+class veiculo:
+    def __init__(self,marca,modelo,ano,placa):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.placa = placa
+
+cadastro = []
+x = 0
