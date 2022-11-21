c3d80a77b00ef56380dcfb1f99efe78bc5c13065
Gustavo Ferreira <gfmb94@gmail.com>
2022-11-21 20:51:06 -0300

    Adicionando interface com usuario e ajustes

--- Aula02/CadastroVeiculo.py
+++ Aula02/CadastroVeiculo.py
@@ -12,22 +12,37 @@ class veiculo:
 cadastro = []
 x = 0
 
+def clear(): os.system('clear')
 
 def tela():
-    def clear(): return os.system('clear')
     clear()
-    print("-------------------------------")
-    print("------CADASTRO DE VEICULO------")
-    print("-------------------------------\n")
+    print("-------------------------------------")
+    print("---------CADASTRO DE VEICULO---------")
+    print("-------------------------------------\n")
 
     cadastro.sort(key=lambda x: x.marca)
 
-    print("{:<10} {:<19} {:<6} {:<8}".format(
+    print("{:<12} {:<8} {:<6} {:<8}".format(
         "Marca", "Modelo", "Ano", "Placa"))
+    print("-------------------------------------")
 
     y = 0
     while (y <= x):
         for v in cadastro:
-            print("{:<10} {:<10} {:<6} {:<8}".format(
+            print("{:<12} {:<8} {:<6} {:<8}".format(
                 v.marca, v.modelo, v.ano, v.placa))
             y += 1
+
+
+res = int(input("\nDeseja cadastrar um novo veículo? Sim(1) / Não(0): "))
+
+while (res == 1):
+    cadMarca = input("\nDigite a marca do veículo: ")
+    cadModelo = input("Digite o modelo do veículo: ")
+    cadAno = input("Digite o ano do veículo: ")
+    cadPlaca = input("Digite a placa do veículo: ")
+
+    cadastro.append(veiculo(cadMarca, cadModelo, cadAno, cadPlaca))
+    tela()
+    x += 1
+    res = int(input("\nDeseja cadastrar um novo veículo? Sim(1) / Não(0): "))
